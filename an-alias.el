85b3a57f4f6c1bb1b4505eab07c7e2ab2b52fc35
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-21 20:09:23 +0000

    Try to use consistent naming for helpers.

---
+++ an-alias.el
@@ -0,0 +1,135 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; string helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro an/string:set (str v)
+  `(setq k ,str ,v))
+
+(defmacro an/string:concat(str  &rest s)
+  `(setq ,str (concat ,str ,@s)))
+
+(defun an/string:repeat(c n)
+  (let ((r ""))
+  (while (> n 0)
+    (an/string:concat r c)
+    (setq n (- n 1)))r))
+
+(defun an/string:ltrim (str)
+  (let ((trim-pos (string-match "\\s +$" str)))
+    (if trim-pos
+        (substring str 0 trim-pos)
+      str)))
+
+(defun an/string:empty (str)
+  (equalp (length (an/string:trim str)) 0))
+
+(defun an/string:trim (str)
+  (an/string:rtrim (an/string:ltrim str)))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; list helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun an/list:split (pos list)
+  (let ((first) (second list) (count 0 ))
+      (while (< count pos)
+            (setq first (append first (list (car second))))
+            (setq second (cdr second))
+            (setq count (+ count 1)))      
+      (list first second)))
+
+(defun an/list:join(lst)
+  (mapconcat 'identity lst " , "))
+
+(defun an/list:filter (fn l)
+  "Filter a list using a predicate"
+  (if (not l)
+      '()
+    (if (funcall fn (car l))                 
+        (cons (car l) 
+              (an/list:filter fn (cdr l)))
+      (an/list:filter fn (cdr l)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; buffer helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun an/buffer:clear-current-buffer()
+  (an/buffer:clear (current-buffer)))
+
+(defun an/buffer:clear (&optional buffer)
+  (if (not buffer)
+      (setq buffer (current-buffer)))
+  (with-current-buffer (get-buffer buffer)
+    (if (> (point-max) 1)
+        (delete-region (point-min) (point-max)))))
+
+(defmacro an/buffer:run-in-scratch(&rest body)
+  `(with-current-buffer (get-buffer-create "*scratch*")
+     (kill-region  (point-min) (point-max))
+     ,@body
+     (switch-to-buffer "*scratch*")
+     (goto-char 0)))
+
+(defun an/buffer:insert-scratch (&optional clearp)
+  (interactive "p")
+  (insert-buffer (get-buffer "*scratch*"))
+  (if clearp
+      (an/buffer:clear-scratch)))
+
+(defun an/buffer:list ()
+  (mapcar (lambda (b) (substring-no-properties (buffer-name b))) (buffer-list)))
+
+(defun an/buffer:match (regexp)
+    (remove-if-not (lambda(s) (string-match regexp s)) (an/buffer:list)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; directory helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'find-lisp)
+
+(defalias  'an/file:find-files-recurse 'find-lisp-find-files)
+
+(defun an/false-predicate(&rest arg) nil)
+
+(defun an/file:find-subdir(directory regexp)
+  "Get list of DIRECTORY names matching REGEXP"
+ (an/list:filter 'file-directory-p (directory-files directory t regexp nil)))
+
+(defun an/file:find-files (directory regexp)
+  "Find files under DIRECTORY,  that match REGEXP."
+  (let ((file-predicate      'find-lisp-default-file-predicate)
+	(directory-predicate 'an/false-predicate)
+	(find-lisp-regexp regexp))
+    (find-lisp-find-files-internal
+     directory
+     file-predicate
+     directory-predicate)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; shell helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun an/shell:visit-buffer-dir()
+  "Try to go to relevant directory in *shell*"
+  (interactive)
+  (let* ((pwd (shell-command-to-string "pwd"))
+        (chdir-cmd (format "cd %s\r" pwd))
+        (file-buffer (current-buffer))
+        (shell-buffer (get-buffer "*shell*"))
+        (shell-process (get-buffer-process shell-buffer))
+        )
+    (process-send-string shell-process chdir-cmd)
+    (pop-to-buffer shell-buffer t)
+    (comint-clear-buffer)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; window helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun an/windows:swap()
+  "Swap buffers in `selected-window' and `other-window'"
+  (interactive)
+  (let* ((w1 (selected-window))
+        (w2 (other-window 1))
+        (b1 (window-buffer w1))
+        (b2 (window-buffer w2)))
+    (set-window-buffer w1 b2)
+    (set-window-buffer w2 b1)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
