a1cf851e4a34480cb515afc8d2a85d8cd970db53
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-05-16 01:18:57 +0000

    Move general purpose stuff in.

--- an-alias.el
+++ an-alias.el
@@ -79,7 +79,43 @@
   (mapcar (lambda (b) (substring-no-properties (buffer-name b))) (buffer-list)))
 
 (defun an/buffer:match (regexp)
-    (remove-if-not (lambda(s) (string-match regexp s)) (an/buffer:list)))
+  (remove-if-not (lambda(s) (string-match regexp s)) (an/buffer:list)))
+
+(defun an/buffer:fetch-lines(n)
+  (let ((reg-begin nil)
+        (reg-end nil))
+  (save-excursion
+    (goto-char (line-beginning-position))
+    (setf reg-begin (point)) 
+    (forward-line n)
+    (goto-char (line-end-position))
+    (setf reg-end (point))
+    ;; AN - 5/14
+    ;;    (if (equal (point) reg-begin)
+    ;;        (setf reg-end (line-end-position))
+    ;;      (setf reg-end (point))
+    )
+  (forward-line n) ;; change buffer state so curser is n-lines ahead
+  (let ((region (buffer-substring-no-properties reg-begin reg-end)))
+    (split-string region "\n" t))))
+
+(defun an/buffer:fetch-line()
+  (car (an/buffer:fetch-lines 1)))
+
+(defun an/buffer:num-lines()
+  (count-lines (point-min) (point-max)))
+
+(defun an/vector-list(ls)
+  (loop with v = (make-vector (length ls) 0)
+        for l in ls 
+        for i = 0 then (+ i 1)
+        do (aset v i l)
+        finally (return v)))
+
+(defun an/buffer:fetch-lines-as-numbers()
+  (an/vector-list
+   (mapcar 'string-to-number
+           (split-string (an/buffer:fetch-line) " "))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; directory helpers
@@ -147,4 +183,47 @@
         (b2 (window-buffer w2)))
     (set-window-buffer w1 b2)
     (set-window-buffer w2 b1)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; vectors - helpers for dealing with vectors
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; table - helpers for two dimension vector.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defstruct table/position row column)
+
+(defun table/make(rows cols cell-value)
+  (loop repeat rows vconcat
+        (vector (loop repeat cols vconcat (make-vector 1 cell-value)))))
+
+(defun table/nrows(table)
+  (length table))
+
+(defun table/ncols(table)
+  (length (aref table 0)))
+
+(defmacro table/at (table i j)
+  `(aref (aref ,table ,i) ,j))
+
+(defmacro table/setf(m i j value)
+  `(setf (aref (aref ,m ,i ) ,j)  ,value))
+
+(defmacro table/at-position(table pos)
+  `(table/at ,table  (g/position-row ,pos)
+             (g/position-column ,pos)))
+
+(defmacro an/swapf(r1 r2)
+  `(let ((temp nil))
+     (setf  temp ,r2)
+     (setf ,r2 ,r1)
+     (setf ,r1 temp)))
+
+(defun an/vector-swap (v p1 p2)
+  (an/swapf (aref v p1) (aref v p2)))
+
+(defun table/swap-rows(table r1 r2)
+  (loop for c from 0 below (g/table-ncols table) do
+        (an/swapf (table/at table r1 c) (table/at table r2 c))))
+
